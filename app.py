b347733c264c073515353fe4a90ad058d1d73433
MOHAMMAD AKIF AKHTAR <mohammadakifakhtar@gmail.com>
2026-07-14 20:47:49 +0530

    Ready for Render deployment

--- app.py
+++ app.py
@@ -79,5 +79,7 @@ def predict():
     return redirect(url_for("index"))
 
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
